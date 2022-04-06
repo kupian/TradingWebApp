f6bc6bd27de96445922a17c20444820b20fd3720
kupian <alexcarr1113@outlook.com>
2022-04-06 16:19:54 +0100

    Candlestick chart component created

--- client/src/components/stockChart.js
+++ client/src/components/stockChart.js
@@ -1,11 +1,57 @@
-import React from 'react'
-import Plot from "react-plotly.js"
+import { React, useState } from 'react';
+import Chart from "react-apexcharts";
 
-export default function stockChart() {
+export default function StockChart() {
+
+    const IEX_KEY = process.env.REACT_APP_IEX_KEY;
+
+    async function getStockData(symbol) {
+    const url = `https://cloud.iexapis.com/stable/stock/${symbol}/chart/5d?token=${IEX_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+    }
+
+    function handleChange(e) {
+        setSymbol(e.target.value);
+    }
+
+    function handleBtnClick() {
+        getStockData(symbol).then(data => {
+            console.log(data)
+        });
+    }
+
+    const [symbol, setSymbol] = useState("");
+    const [chart, setChart] = useState({
+        options: {
+            chart: {
+                id: "candlestick"
+            },
+        },
+        series: [{
+            data: [{
+                x: new Date(2022, 3, 4),
+                y: [53.66, 54.99, 51.35, 52.95]
+            },
+            {
+                x: new Date(2022, 3, 5),
+                y: [52.95, 55.52, 50.78, 53.6]
+            }]
+        }]
+    })
   return (
     <div>stockChart
+        <Chart
+            options={chart.options}
+            series={chart.series}
+            type="candlestick"
+            width="800"
+            height="200" />
 
-      </div>
+            <input type="text" value={symbol} onChange={handleChange} />
+            <button onClick={handleBtnClick}>Get stock data</button>
+    </div>
     
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import {
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Test from './pages/Test';
-import stockChart from './components/stockChart';
+import StockChart from './components/StockChart';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,7 +19,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/chart" element={<stockChart />} />
+          <Route path="/chart" element={<StockChart />} />
           <Route path="/test2" element={<Test />} />
         </Route>
       </Routes>
