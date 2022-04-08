721f4c59d6fc55463516d0fac0332bdfb87b8f53
Alex <alexcarr1113@outlook.com>
2022-04-08 22:13:09 +0100

    messages map

--- client/src/components/ChatFeed.js
+++ client/src/components/ChatFeed.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import {Form, Button, Stack, Row, Col, FormControl} from 'react-bootstrap'
+import { Form, Button, Stack, Row, Col, FormControl } from 'react-bootstrap'
 
-// A placeholder - needs another database table to store the chat messages and all that jazz
-const messages = ["hello","banana is good today","apple is shit"]
 
 export default function ChatFeed() {
+
+  // A placeholder - needs another database table to store the chat messages and all that jazz
+  const messages = ["hello", "banana is good today", "apple is shit", "BUY CUMCOIN"]
+
+
+  messages.map(message => {
+    <div className="bg-light border ms-auto">{message}</div>
+  })
+
   return (
     <Stack>
-        <Row>
-        <div className="bg-light border ms-auto">{messages[0]}</div>
-        <div className="bg-light border ms-auto">{messages[1]}</div>
-        <div className="bg-light border ms-auto">{messages[2]}</div>
-        </Row>
-        <Row>
-            <Col>
-                <Form.Control type="text" placeholder="Type a message..." />
-            </Col>
-            <Col>
-                <Button variant="primary" type="submit">Submit</Button>
-            </Col>
-        </Row>
+      <Row>
+        {messages.map((message, i) => {
+          return (
+            <div key={i} className="bg-light border ms-auto">{message}</div>
+          )
+        })}
+      </Row>
+      <Row>
+        <Col>
+          <Form.Control type="text" placeholder="Type a message..." />
+        </Col>
+        <Col>
+          <Button variant="primary" type="submit">Submit</Button>
+        </Col>
+      </Row>
     </Stack>
   )
 }
