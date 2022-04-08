fda56fa2d7be4d1c960cea99180489c942f6bf19
willtree8 <willtree8@gmail.com>
2022-04-08 20:39:18 +0100

    started blocking out main page ui

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import {Row, Col, Card, Container} from 'react-bootstrap'
+import Watchlist from '../components/Watchlist'
+import Leaderboard from '../components/Leaderboard'
+import Portfolio from '../components/Portfolio'
+import ChatFeed from '../components/ChatFeed'
 
 export default function test() {
   return (
-    <div>Homepage</div>
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Leaderboard />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Watchlist symbol="AAPL" price="50"/>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Portfolio />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <ChatFeed />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
