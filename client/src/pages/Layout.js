ad964b1485fac07f411f6c0d3ffcff28840c7132
Alex <alexcarr1113@outlook.com>
2022-04-09 00:05:42 +0100

    layout changes

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -3,17 +3,15 @@ import {Outlet} from 'react-router-dom';
 import Navigation from '../pages/Navigation';
 import "./Layout.css";
 import config from 'config';
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Layout() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  const [lobbyCode, setLobbyCode] = useState("");
   const DEV_MODE = config["dev-mode"];
-  const [username, setUsername] = useState("");
   let devMode;
-  useEffect( () => {
-    setUsername(localStorage.getItem("username"));
-  }, []);
 
   if (DEV_MODE) { devMode = <p id='dev-mode'>DEV MODE IS ACTIVE, ALL DATA IS FAKE</p>; }
-
 
   return (
     <div>
