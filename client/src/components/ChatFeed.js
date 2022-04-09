796ba8411a950ca80a88e230cd86bab96a8003c4
willtree8 <willtree8@gmail.com>
2022-04-09 01:05:25 +0100

    added comments

--- client/src/components/ChatFeed.js
+++ client/src/components/ChatFeed.js
@@ -7,14 +7,10 @@ export default function ChatFeed() {
   // A placeholder - needs another database table to store the chat messages and all that jazz
   const messages = [{sender:"pussy-slayer-9000", body: "hello"}, {sender:"urmum2003", body: "banana is good today"}, {sender:"cooldude901", body: "apple is shit"}, {sender:"xdkid21", body: "BUY CUMCOIN"}]
 
-
-  messages.map(message => {
-    <div className="bg-light border ms-auto">{message}</div>
-  })
-
   return (
     <Stack>
       <Row>
+        // This is the chat feed - needs to be populated with the messages from the database
         {messages.map((message, i) => {
           return (
             <div key={i} className="bg-light border ms-auto"><strong>{message.sender}: </strong>{message.body}</div>
