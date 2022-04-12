411dc80b2ca522a4cf480ccff9459d1df8cea29e
kupian <alexcarr1113@outlook.com>
2022-04-12 17:53:57 +0100

    profile changes

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Profile() {
-    const { user, isAuthenticated, isLoading } = useAuth0();
+    const { user, isAuthenticated, isLoading } = [props.user, props.isAuthenticated, props.isLoading];
 
     if (isLoading) return (<div>Loading...</div>);
 
